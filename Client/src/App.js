ca6e7d74aae9d76a0b158cca1eb2eba1a9945639
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2024-12-19 16:44:10 +0500

    Home page , navbar, hero section and games section is created

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div>Xephra </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="*" element={<Home />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ Client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Herosection from '../components/Herosection'
+import GamesCards from '../components/GamesCards'
+
+export default function Home() {
+  return (<>
+    <Navbar />
+    <Herosection />
+    <GamesCards />
+    </>
+  )
+}
