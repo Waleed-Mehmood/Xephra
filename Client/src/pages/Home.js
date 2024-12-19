f986a87efca8dbfd86b96426af7531527c458b35
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2024-12-19 17:11:17 +0500

    tournament section (broken)

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Herosection from '../components/Herosection'
 import GamesCards from '../components/GamesCards'
+import Tournaments from '../components/Tournaments'
 
 export default function Home() {
   return (<>
     <Navbar />
     <Herosection />
     <GamesCards />
+    {/* <Tournaments /> */}
     </>
   )
 }
