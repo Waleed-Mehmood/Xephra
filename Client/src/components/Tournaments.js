e401e9f66cb7948dc1c3d36085f3453f43736371
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2024-12-20 17:24:28 +0500

    minor changes

--- Client/src/components/Tournaments.js
+++ Client/src/components/Tournaments.js
@@ -75,9 +75,9 @@ const Tournaments = () => {
   ];
 
   return (
-    <div className="container mx-auto py-16 px-4 bg-[#232122]">
+    <div className=" mx-auto py-16 px-4 bg-[#232122]">
       <h2 className="text-3xl font-bold text-center mb-8 text-[#b6a99a] py-6">UPCOMING TOURNAMENTS</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {tournaments.map((tournament, index) => (
           <TournamentCard key={index} {...tournament} />
         ))}
