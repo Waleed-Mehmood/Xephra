17e8c996c2bcfad0201378a5af9c01ece19356ec
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2024-12-23 11:39:19 +0500

    few changes in folder structure

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Signup from "./components/Signup";
+import Signup from "./pages/Signup";
 
 export default function App() {
   return (
--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Herosection from '../components/Herosection'
-import GamesCards from '../components/GamesCards'
-import Tournaments from '../components/Tournaments'
-import Prices from '../components/Prices'
+import Navbar from '../components/HomePageComponents/Navbar'
+import Herosection from '../components/HomePageComponents/Herosection'
+import GamesCards from '../components/HomePageComponents/GamesCards'
+import Tournaments from '../components/HomePageComponents/Tournaments'
+import Prices from '../components/HomePageComponents/Prices'
 
 export default function Home() {
   return (<>
