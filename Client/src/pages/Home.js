d3a3528a6d283f40ec6f3dee313dea7b3227c993
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2025-01-22 17:41:07 +0500

    home page changes

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,17 +1,44 @@
-import React from 'react'
-import Navbar from '../components/HomePageComponents/Navbar'
-import Herosection from '../components/HomePageComponents/Herosection'
-import GamesCards from '../components/HomePageComponents/GamesCards'
-import Tournaments from '../components/HomePageComponents/Tournaments'
-import Prices from '../components/HomePageComponents/Prices'
+import { React, useRef } from "react";
+import Navbar from "../components/HomePageComponents/Navbar";
+import Herosection from "../components/HomePageComponents/Herosection";
+import GamesCards from "../components/HomePageComponents/GamesCards";
+import Tournaments from "../components/HomePageComponents/Tournaments";
+import Prices from "../components/HomePageComponents/Prices";
 
 export default function Home() {
-  return (<>
-    <Navbar />
-    <Herosection />
-    <GamesCards />
-    <Tournaments />
-    <Prices />
+  const homeRef = useRef(null);
+  const gameRef = useRef(null);
+  const tournamentsRef = useRef(null);
+  const priceRef = useRef(null);
+
+  const scrollToSection = (sectionRef) => {
+    sectionRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+  return (
+    <>
+      <Navbar
+        scrollToSection={scrollToSection}
+        refs={{
+          homeRef,
+          gameRef,
+          tournamentsRef,
+          priceRef,
+        }}
+      />
+      <div ref={homeRef}>
+        <Herosection />
+      </div>
+      <div ref={gameRef}>
+        <GamesCards />
+      </div>
+      <div ref={tournamentsRef}>
+        <Tournaments />
+      </div>
+      <div ref={priceRef}>
+        <Prices />
+      </div>
     </>
-  )
+  );
 }
