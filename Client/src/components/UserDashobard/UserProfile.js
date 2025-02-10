dc96dc2f144fc5121af96e09e0202e9a2a3899d7
Iyisku <nofuiyisku@gmail.com>
2025-02-10 17:45:33 +0500

    profilepagev2

--- Client/src/components/UserDashobard/UserProfile.js
+++ Client/src/components/UserDashobard/UserProfile.js
@@ -178,213 +178,256 @@ const UserProfile = ({ dark, profile }) => {
   if (loading) return <Loading />;
 
   return (
-    <div
-      className={`${styles.userProfile} ${
-        dark ? "bg-[#69363F]" : "bg-[#232122]"
-      }`}
-    >
-      <h1>User Profile</h1>
-      <div className={styles.profileImageContainer}>
-        <div className={styles.profileImageWrapper}>
-          <img
-            src={
-              profileImageView
-                ? profileImageView
-                : profileData?.profileImage
-                ? `http://localhost:5000/${profile?.profileImage}`
-                : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvyKxD07vzVrTXqVFK0myyV8KT99ZWBNNwGA&s"
-            }
-            alt="Profile"
-            className={styles.avatar}
-          />
-          <label htmlFor="profileImage" className={styles.cameraIconWrapper}>
-            <FaCamera className={styles.cameraIcon} />
-            <input
-              type="file"
-              id="profileImage"
-              accept="image/*"
-              onChange={handleImageChange}
-              style={{ display: "none" }}
-            />
-          </label>
+    <>
+      {/* [#4d2930] */}
+      <div>
+        <div className="bg-gradient-to-r from-[#3e2027] to-[#2e1c20]  p-4 rounded-2xl gap-4 shadow-lg mb-2 sm:m-4">
+          <div className="flex flex-col items-end gap-0">
+            <h1 className="text-xl text-white">Rank : 12</h1>
+            <h2 className=" text-white">Score: 12</h2>
+          </div>
+
+          <div className={styles.profileImageContainer}>
+            <div className={styles.profileImageWrapper}>
+              <img
+                src={
+                  profileImageView
+                    ? profileImageView
+                    : profileData?.profileImage
+                    ? `http://localhost:5000/${profile?.profileImage}`
+                    : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvyKxD07vzVrTXqVFK0myyV8KT99ZWBNNwGA&s"
+                }
+                alt="Profile"
+                className={styles.avatar}
+              />
+              <label
+                htmlFor="profileImage"
+                className={styles.cameraIconWrapper}
+              >
+                <FaCamera className={styles.cameraIcon} />
+                <input
+                  type="file"
+                  id="profileImage"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  style={{ display: "none" }}
+                />
+              </label>
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="text-3xl ml-10">{profileData.username}</label>
+              <label className="text-xl ml-10">{profileData.bio}</label>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.profileForm}>
-        <label>
-          <FaUser /> Username
-        </label>
-        <input
-          type="text"
-          name="username"
-          value={profileData?.username || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+        <div className={styles.profileForm}>
+          <div className="grid grid-cols-1 lg:flex lg:justify-between gap-5 sm:ml-4">
+            {/* Username */}
+            <div className="grid grid-cols-1 gap-2 w-full lg:w-90">
+              <label className="flex items-center gap-2">
+                <FaUser /> Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                value={profileData?.username || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
 
-        <label>
-          <FaUser /> Full Name
-        </label>
-        <input
-          type="text"
-          name="fullName"
-          value={profileData?.fullName || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+            {/* Full Name */}
+            <div className="grid grid-cols-1 gap-2 w-full lg:w-90 ">
+              <label className="flex items-center gap-2">
+                <FaUser /> Full Name
+              </label>
+              <input
+                type="text"
+                name="fullName"
+                value={profileData?.fullName || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
+          </div>
 
-        <label>
-          <FaEnvelope /> Bio/About
-        </label>
-        <textarea
-          name="bio"
-          value={profileData?.bio || ""}
-          onChange={handleChange}
-          className="p-2 rounded border h-32"
-        ></textarea>
+          <div className="grid grid-cols-1 lg:flex lg:justify-between gap-5 sm:ml-4">
+            {/* Email */}
+            <div className="grid grid-cols-1 gap-2 w-full lg:w-90">
+              <label className="flex items-center gap-2">
+                <FaEnvelope /> Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={profileData?.email || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
 
-        <label>
-          <FaEnvelope /> Email
-        </label>
-        <input
-          type="email"
-          name="email"
-          value={profileData?.email || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+            {/* Phone Number */}
+            <div className="grid grid-cols-1 gap-2 w-full lg:w-90">
+              <label className="flex items-center gap-2">
+                <FaPhone /> Phone Number
+              </label>
+              <input
+                type="text"
+                name="phoneNumber"
+                value={profileData?.phoneNumber || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
+          </div>
 
-        <label>
-          <FaPhone /> Phone Number
-        </label>
-        <input
-          type="text"
-          name="phoneNumber"
-          value={profileData?.phoneNumber || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+          {/* Added address field */}
+          <div className="grid grid-cols-1 lg:flex lg:justify-between gap-5 sm:ml-4">
+            <div className="grid grid-cols-1 gap-2 w-full lg:w-90">
+              <label className="flex items-center gap-2">
+                <FaMapMarkedAlt /> Address
+              </label>
+              <input
+                type="text"
+                name="address"
+                value={profileData?.address || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
 
-        {/* Added address field */}
-        <label>
-          <FaMapMarkedAlt /> Address
-        </label>
-        <input
-          type="text"
-          name="address"
-          value={profileData?.address || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+            {/* Age Field - Single column on small screens, beside Address on large screens */}
+            <div className="grid grid-cols-1 w-full lg:w-90 gap-2">
+              <label className="flex items-center gap-2">
+                <FaCalendarAlt /> Age
+              </label>
+              <input
+                type="number"
+                name="age"
+                value={profileData?.age || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
+          </div>
 
-        <label>
-          <FaCalendarAlt /> Age
-        </label>
-        <input
-          type="number"
-          name="age"
-          value={profileData?.age || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+          <div className="grid grid-cols-1 lg:flex lg:justify-between gap-5 sm:ml-4">
+            {/* City Field - Single column on small screens, beside Address and Age on large screens */}
+            <div className="grid grid-cols-1 w-full lg:w-90 gap-2">
+              <label className="flex items-center gap-2">
+                <FaCity /> City
+              </label>
+              <input
+                type="text"
+                name="locationCity"
+                value={profileData?.locationCity || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
 
-        <label>
-          <FaCity /> City
-        </label>
-        <input
-          type="text"
-          name="locationCity"
-          value={profileData?.locationCity || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+            {/* Country Field - Single column on small screens, beside City on large screens */}
+            <div className="grid grid-cols-1 w-full lg:w-90 gap-2">
+              <label className="flex items-center gap-2">
+                <FaCity /> Country
+              </label>
+              <input
+                type="text"
+                name="locationCountry"
+                value={profileData?.locationCountry || ""}
+                onChange={handleChange}
+                className="p-2 rounded border w-full"
+              />
+            </div>
+          </div>
 
-        <label>
-          <FaCity /> Country
-        </label>
-        <input
-          type="text"
-          name="locationCountry"
-          value={profileData?.locationCountry || ""}
-          onChange={handleChange}
-          className="p-2 rounded border"
-        />
+          <label className="sm:ml-4">
+            <FaEnvelope /> Bio/About
+          </label>
+          <textarea
+            name="bio"
+            value={profileData?.bio || ""}
+            onChange={handleChange}
+            className="p-2 rounded border h-32 sm:ml-4"
+          ></textarea>
 
-        <label>
-          <FaGamepad /> Favourite Games
-        </label>
-        <div className="flex flex-col space-y-2">
-          <div className="flex space-x-2">
-            <input
-              type="text"
-              value={newGame}
-              onChange={handleGameChange}
-              className="p-2 rounded border flex-1"
-            />
+          <label className="sm:ml-4">
+            <FaGamepad /> Favourite Games
+          </label>
+          <div className="flex flex-col space-y-2 sm:ml-4">
+            <div className="flex space-x-2">
+              <input
+                type="text"
+                value={newGame}
+                onChange={handleGameChange}
+                className="p-2 rounded border flex-1"
+              />
+              <button
+                type="button"
+                onClick={handleAddGame}
+                className={`p-2 text-white rounded ${
+                  dark
+                    ? "bg-[#302B27] hover:bg-[#49413C]"
+                    : "bg-[#854951] hover:bg-[rgb(161,93,102)]"
+                }`}
+              >
+                Add Game
+              </button>
+            </div>
+
+            <ul className="space-y-1">
+              {profileData.favoriteGames.length > 0 ? (
+                profileData.favoriteGames.map((game, index) => (
+                  <li key={index} className="flex items-center">
+                    <span className="text-[#B6A99A] text-md font-bold">
+                      {game}
+                    </span>
+                    <button
+                      type="button"
+                      onClick={() => handleRemoveGame(game)}
+                      className="text-red-500 hover:text-red-700 ml-4"
+                    >
+                      Remove
+                    </button>
+                  </li>
+                ))
+              ) : (
+                <p className="text-gray-500">No favorite games added yet.</p>
+              )}
+            </ul>
+          </div>
+
+          {/* Show message if no changes are detected */}
+          {message && <div className={styles.message}>{message}</div>}
+
+          {/* Conditionally render the button */}
+          {profile ? (
             <button
-              type="button"
-              onClick={handleAddGame}
-              className={`p-2 text-white rounded ${
+              className={`${styles.uploadBtn} ${
                 dark
                   ? "bg-[#302B27] hover:bg-[#49413C]"
                   : "bg-[#854951] hover:bg-[#A15D66]"
               }`}
+              onClick={handleProfileUpdateClick} // Handle update button click
             >
-              Add Game
+              Update
             </button>
-          </div>
-
-          <ul className="space-y-1">
-            {profileData.favoriteGames.length > 0 ? (
-              profileData.favoriteGames.map((game, index) => (
-                <li key={index} className="flex items-center">
-                  <span className="text-[#B6A99A] text-md font-bold">
-                    {game}
-                  </span>
-                  <button
-                    type="button"
-                    onClick={() => handleRemoveGame(game)}
-                    className="text-red-500 hover:text-red-700 ml-4"
-                  >
-                    Remove
-                  </button>
-                </li>
-              ))
-            ) : (
-              <p className="text-gray-500">No favorite games added yet.</p>
-            )}
-          </ul>
+          ) : (
+            <button
+              className={`${styles.uploadBtn} ${
+                dark
+                  ? "bg-[#302B27] hover:bg-[#49413C]"
+                  : "bg-[#854951] hover:bg-[#A15D66]"
+              }`}
+              onClick={handleCreate} // Create the profile if it doesn't exist
+            >
+              Create Profile
+            </button>
+          )}
         </div>
-
-        {/* Show message if no changes are detected */}
-        {message && <div className={styles.message}>{message}</div>}
-
-        {/* Conditionally render the button */}
-        {profile ? (
-          <button
-            className={`${styles.uploadBtn} ${
-              dark
-                ? "bg-[#302B27] hover:bg-[#49413C]"
-                : "bg-[#854951] hover:bg-[#A15D66]"
-            }`}
-            onClick={handleProfileUpdateClick} // Handle update button click
-          >
-            Update
-          </button>
-        ) : (
-          <button
-            className={`${styles.uploadBtn} ${
-              dark
-                ? "bg-[#302B27] hover:bg-[#49413C]"
-                : "bg-[#854951] hover:bg-[#A15D66]"
-            }`}
-            onClick={handleCreate} // Create the profile if it doesn't exist
-          >
-            Create Profile
-          </button>
-        )}
       </div>
-    </div>
+    </>
   );
 };
 
