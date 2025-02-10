5de3babc9c8479bd104a42f0a044778c27da1879
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2025-02-10 14:10:42 +0500

    ranking approval api

---
+++ Backend/controllers/ranking.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const UserSubmission = require("../models/UserSubmission");
+
+exports.postRankingApproval = async (req, res) => {
+  try {
+    const { eventId, userId, rank, score } = req.body;
+    const screenshot = req.file ? `uploads/${req.file.filename}` : null;
+    if (!eventId || !userId) {
+      return res.status(400).json({
+        message: "EventId and userId are required!",
+      });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Screenshot is required",
+      });
+    }
+
+    const newSubmission = new UserSubmission({
+      eventId,
+      userId,
+      rank: rank || null,
+      score: score || null,
+      screenshot,
+      createdAt: Date.now(),
+    });
+
+    const savedSubmission = await newSubmission.save();
+
+    res.status(200).json({
+      message: "Submission created successfully",
+      data: savedSubmission,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
+  }
+};
