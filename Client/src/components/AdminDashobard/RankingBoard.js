1f44c7d94c00aa83282fa0d20899eaaf3c10b874
Waleed Mehmood <80235010+Waleed-Mehmood@users.noreply.github.com>
2025-02-19 17:27:12 +0500

    minor changes

--- Client/src/components/AdminDashobard/RankingBoard.js
+++ Client/src/components/AdminDashobard/RankingBoard.js
@@ -89,9 +89,9 @@ const RankingBoard = ({ dark }) => {
   );
 
   return (
-    <div className={`min-h-screen p-8 shadow-2xl shadow-gray-950 rounded-xl backdrop-blur-sm ${
+    <div className={`min-h-screen p-8 shadow-2xl shadow-gray-950 rounded-xl backdrop-blur-sm bg-[#492f3418] bg-opacity-[.06] ${
       dark
-        ? "bg-[#492f3418] bg-opacity-[.06]":"bg-[#232122]"}`}>
+        ? "":""}`}>
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center mb-6 bg-gradient-to-r from-[#D19F43] via-[#d1a759] to-[#eb9a0d] bg-clip-text text-transparent">
         Ranking Board
       </h1>
