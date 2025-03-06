6ffc03de5c9c0ae2ec48141a46b6de299c1f8382
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2025-03-06 14:51:50 +0500

    set headers

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,19 @@ const corsOptions = {
 
 
 app.use(cors(corsOptions)); 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://xephra.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
 
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 
 app.use(passport.initialize());
 
