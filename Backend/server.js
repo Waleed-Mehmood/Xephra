1c75521051a7ee8e74f38e747ac030d9387fef0d
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2025-03-06 15:01:57 +0500

    vercel config

--- Backend/server.js
+++ Backend/server.js
@@ -37,7 +37,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions)); 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://xephra.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
